43a93f653787f19ad0e97f1c956193de320aed02
ram s <raammuu@amazon.com>
2025-09-01 21:01:44 +0530

    Intermediate -  Count the number of factors

---
+++ src/main/java/scalar/intermediate/CountFactor.java
@@ -0,0 +1,39 @@
+/*Question: A program to count the factors of a number
+Example
+Number: 12
+Factors: 1, 2, 3, 4, 6, 12
+Total count: 6
+ */
+
+package scalar.intermediate;
+
+public class CountFactor {
+    public int getNoOfFactors(int n) {
+        int count = 0;
+        for (int i = 1; i * i <= Math.abs(n); i++) {
+            if (n % i == 0) {
+                if (i == n / i) {
+                    count++;
+                } else {
+                    count += 2;
+                }
+            }
+        }
+        return count;
+    }
+}
+
+/* Explaination
+
+Why Use √n Optimization?
+ - Instead of checking all numbers from 1 to n, we only check numbers up to √n because:
+ - If i is a factor of n, then n/i is also a factor.
+    Example: For 36, checking until 6 is enough because pairs (6, 6) and (2, 18) cover all factors.
+This reduces unnecessary iterations.
+
+🔹 Time Complexity
+     We iterate only until √n, so complexity is O(√n).
+
+🔹 Space Complexity
+       We use only a few variables (n, count, i), so space complexity is O(1).
+ */
